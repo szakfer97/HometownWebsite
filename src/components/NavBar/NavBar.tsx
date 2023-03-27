b30d24748531a0bc7533c075d06a644e4e5ebbb3
szakferi <szakfer97@gmail.com>
2023-03-27 18:07:20 +0300

    Added NavBar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <div>
+      <ul className="navbar navbar-light bg-light">
+        <li className="navbar-brand">Community</li>
+        <li className="navbar-brand">Gallery</li>
+        <li className="navbar-brand">History</li>
+        <li className="navbar-brand">Main Attractions</li>
+      </ul>
+    </div>
+  );
+}
