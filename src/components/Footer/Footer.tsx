ae2d972d021413756de218cc499fb7b31526c2c9
szakferi <szakfer97@gmail.com>
2023-04-04 20:52:42 +0300

    Added Footer elements

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,14 @@
 export default function Footer() {
-  return <div className="h1">Footer</div>;
+  return (
+    <div className="h1">
+      <div>
+        <span>Footer</span>
+        <span>Terms and Conditions</span>
+        <span>Contact us</span>
+      </div>
+      <div>
+        <span>Satu Mare 2023</span>
+      </div>
+    </div>
+  );
 }
