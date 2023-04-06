0070becd3260fe3f9eab7b8c5eea7fddd701efba
szakferi <szakfer97@gmail.com>
2023-04-06 21:05:33 +0300

    Added route paths

--- src/constants/RoutePaths.ts
+++ src/constants/RoutePaths.ts
@@ -1,1 +1,5 @@
 export const HOMEPAGE = '/';
+export const COMMUNITY = '/community';
+export const GALLERY = '/gallery';
+export const HISTORY = '/history';
+export const MAIN_ATTRACTIONS = '/main-attractions';
